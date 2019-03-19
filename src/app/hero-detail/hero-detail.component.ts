f9fe921b51c6f8712f7523c5a193dc7bd1bfa2d1
icerda <icerda@create-cook-it.com.mx>
2019-03-19 17:53:25 -0600

    Routing

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
+import {HeroService} from '../hero.service';
+import {Hero} from '../hero';
+
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,9 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroDetailComponent implements OnInit {
 
-  constructor() { }
+  hero: Hero;
 
-  ngOnInit() {
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
